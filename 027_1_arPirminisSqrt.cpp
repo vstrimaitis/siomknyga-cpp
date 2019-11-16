886ef47fc67e95d3c2b2cc175f65875926ad1e8f
Vytautas Strimaitis <vstrimaitis@gmail.com>
2019-11-16 13:51:53 +0200

    Fixed typo

--- 027_1_arPirminisSqrt.cpp
+++ 027_1_arPirminisSqrt.cpp
@@ -1,7 +1,7 @@
 bool pirminis(long long n) {
     if(n == 1) return false;
     if(n == 2) return true;
-    if(n%2 != 0) return false;
+    if(n%2 == 0) return false;
 
     for(int d = 3; d*d <= n; d+=2) {
         if(n%d == 0) return false;
