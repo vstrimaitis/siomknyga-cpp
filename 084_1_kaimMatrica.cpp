e27fd6e6467838b1fd42f7b00de9d7abd95cbaf2
Aldas <aldas.lenksas@gmail.com>
2020-11-03 19:37:20 +0200

    Update 084_1_kaimMatrica.cpp
    Co-authored-by: Vytautas Strimaitis <vstrimaitis@gmail.com>

--- 084_1_kaimMatrica.cpp
+++ 084_1_kaimMatrica.cpp
@@ -2,5 +2,6 @@ int n;                    // viršūnių skaičius
 bool briauna[MAXN][MAXN]; // jei briauna[i][j] == true, tai grafe yra briauna tarp viršūnių i ir j
 
 
-// Vertėjo pastaba: pascal kalbos kode grafas pateikiamas kaip struktūra,
+// Pastaba: originaliame pavyzdyje grafas pateikiamas kaip struktūra,
+
 //  tačiau čia kintamuosius ir bool masyvą apsirašome globaliai.
