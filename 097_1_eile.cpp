4baac16a5481211cca33dcd2a1257c326169f43e
unknown <aldas.lenksas@gmail.com>
2020-11-03 20:30:07 +0200

    Updated 097_1_eile.cpp

--- 097_1_eile.cpp
+++ 097_1_eile.cpp
@@ -1,3 +1,17 @@
+/*
+    Pastaba: C++ kalboje jau yra suimplementuota eilė:
+     #include <queue>
+     queue<int> q;
+    Tokia eilė palaiko visas reikiamas operacijas:
+     q.push(x) - įdeda į eilės galą skaičių x
+     q.front() - grąžina eilės priekyje esantį elementą
+     q.pop()   - pašalina iš eilės priekinį elementą
+
+    Visgi, įdomumo dėlei, pateikiame ir eilės kaip struktūros
+    implementaciją, analogišką esančiai knygoje.
+*/
+
+
 struct eile {
     int duom[MAXN];
     int pradzia, pabaiga;
@@ -21,11 +35,3 @@ struct eile {
     }
 
 };
-
-//  Pastaba: C++ kalboje jau yra suimplementuota eilė:
-//   #include <queue>
-//   queue<int> q;
-//  Tokia eilė palaiko visas reikiamas operacijas:
-//   q.push(x) - įdeda į eilės galą skaičių x
-//   q.front() - grąžina eilės priekyje esantį elementą
-//   q.pop() - pašalina iš eilės priekinį elementą
