306de1199850f115c5eb7ceb117cf648e4bef21f
Aldas <aldas.lenksas@gmail.com>
2020-11-14 20:58:28 +0200

    Update 110_1_tiltas.cpp

--- 110_1_tiltas.cpp
+++ 110_1_tiltas.cpp
@@ -2,14 +2,14 @@ bool tiltas (int v, int u) {
     if (briaunuSk[v][u] > 1)
         return false;
     
-        for (int k = 0; k < n; k++)
-            spalva[k] = 0;
+    for (int k = 0; k < n; k++)
+        spalva[k] = 0;
 
-        briaunuSk[v][u]--;
-        briaunuSk[u][v]--;
-        dfs(u);
-        briaunuSk[v][u]++;
-        briaunuSk[u][v]++;
+    briaunuSk[v][u]--;
+    briaunuSk[u][v]--;
+    dfs(u);
+    briaunuSk[v][u]++;
+    briaunuSk[u][v]++;
 
-        return spalva[v] == 0;
+    return spalva[v] == 0;
 }
