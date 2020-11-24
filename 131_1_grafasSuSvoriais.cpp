c2004867418d3e411511a302572f37505bc823fb
unknown <aldas.lenksas@gmail.com>
2020-11-24 10:59:55 +0200

    Dijkstra code translated dijkstra section task with dijkstra section

---
+++ 131_1_grafasSuSvoriais.cpp
@@ -0,0 +1,6 @@
+const int MAXN = ...;      // maksimalus grafo viršūnių skaičius
+const int BEGALINIS = ...; // kažkoks pakankamai didelis skaičius, kuris turėtų būti didesnis
+                           //  už bet kokį įmanomą atstumą, tad atstotų begalybės reikšmę.
+
+int n; // viršūnių skaičius
+int svoris[MAXN][MAXN];
