84c2a2201cfaf9cd55766576118e8a6698e2c97c
Aldas Lenkšas <aldas.lenksas@gmail.com>
2020-11-25 18:54:37 +0200

    Update 131_1_grafasSuSvoriais.cpp

--- 131_1_grafasSuSvoriais.cpp
+++ 131_1_grafasSuSvoriais.cpp
@@ -1,6 +1,9 @@
 const int MAXN = ...;      // maksimalus grafo viršūnių skaičius
-const int BEGALINIS = ...; // kažkoks pakankamai didelis skaičius, kuris turėtų būti didesnis
-                           //  už bet kokį įmanomą atstumą, tad atstotų begalybės reikšmę.
+const int BEGALINIS = ...; 
+/*
+  Konstanta BEGALINIS - kažkoks pakankamai didelis skaičius, kuris turėtų būti didesnis už bet  
+  kokį įmanomą atstumą, tad atstotų begalybės reikšmę. Pavyzdžiui, dažnai naudojama reikšmė 1e9.
+*/
 
 int n; // viršūnių skaičius
 int svoris[MAXN][MAXN];
