5bc2abd80d38ab9b8d26d64aef0fbbfa53aa9986
unknown <aldas.lenksas@gmail.com>
2020-11-26 09:52:25 +0200

    Tree chapter translated

---
+++ 145_1_medis.cpp
@@ -0,0 +1,2 @@
+const int MAXN = ...; // maksimalus medžio viršūnių skaičius
+int pirmine[MAXN];    // kiekvienai viršūnei įsimenama jos pirminė viršūnė
---
+++ 153_1_primoAlgo.cpp
@@ -0,0 +1,48 @@
+const int BEGALINIS = ...; // kažkoks pakankamai didelis skaičius, pavyzdžiui 1e9
+const int MAXN = ...;      // maksimalus viršūnių skaičius
+
+int n;                     // viršūnių skaičius
+int svoris[MAXN][MAXN];
+int pirmine[MAXN];
+vector<int> antrSar[MAXN]; // antrinių viršūnių sąrašas
+bool prijungta[MAXN];
+int kaina[MAXN];
+
+void primo () {
+    // ieškomas medis grąžinamas masyve "pirmine"
+
+    // įrašomos pradinės masyvų reikšmės
+    for (int u = 0; u < n; u++) {
+        kaina[u] = BEGALINIS;
+        pirmine[u] = -1;
+        prijungta[u] = false;
+    }
+
+    int v = 0;
+    kaina[v] = 0; // pradėsime nuo viršūnės su numeriu 0
+
+    while (v != -1) {
+        // jei v != -1, tai rasta viršūnė, kurią galima prijungti
+        prijungta[v] = true;
+
+        for (int u = 0; u < n; u++) { // nagrinėjamos kaimynės
+            if (!prijungta[u] && svoris[v][u] < BEGALINIS && kaina[u] > svoris[v][u]) {
+                // viršūnę u verčiau prijungti prie v
+                kaina[u] = svoris[v][u];
+                pirmine[u] = v;
+            }
+        }
+
+        // randama tolesnė kandidatė - dar neprijungta viršūnė su mažiausia prijungimo kaina
+        v = -1;
+        int minKaina = BEGALINIS;
+        for (int u = 0; u < n; u++) {
+            if (!prijungta[u] && kaina[u] < minKaina) {
+                v = u;
+                minKaina = kaina[u];
+            }
+        }
+
+        // jei jokia viršūnė nerasta, tai v = 0 ir ciklas nutraukiamas
+    }
+}
