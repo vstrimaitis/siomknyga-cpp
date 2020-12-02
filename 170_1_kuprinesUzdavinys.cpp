49bbfd716e64fd3374150663f4186081e23e45f1
unknown <aldas.lenksas@gmail.com>
2020-12-02 08:19:17 +0200

    Updated kuprinesUzdavinys.cpp

--- 170_1_kuprinesUzdavinys.cpp
+++ 170_1_kuprinesUzdavinys.cpp
@@ -5,23 +5,26 @@ int n;
 int S;
 int svoris[MAXN+1];
 int verte[MAXN+1];
-int D[MAXN+1][MAXS+1];
+int dp[MAXN+1][MAXS+1]; // knygoje šis masyvas žymimas "D", tačiau žymėti "dp" yra labiau įprasta
 
 void skaiciuok () {
     // užpildomos kraštinės lentelės  reikšmės
     for (int r = 0; r <= S; r++)
-        D[0][r] = 0;
+        dp[0][r] = 0;
     for (int k = 0; k <= n; k++)
-        D[k][0] = 0;
+        dp[k][0] = 0;
     // užpildoma visa likusi lentelės dalis
     for (int r = 1; r <= S; r++) {
         for (int k = 1; k <= n; k++) {
             if (svoris[k] <= r)
                 // jei k-asis eksponatas tilptų
-                D[k][r] = max (D[k-1][r], verte[k-1] + D[k-1][r-svoris[k]])
+                dp[k][r] = max(
+                    dp[k-1][r],                         // k-ojo daikto neimame - svorį r turime gauti iš pirmų k-1 daiktų
+                    verte[k-1] + dp[k-1][r-svoris[k]]   // k-ąjį daiktą imame - tuomet pridedama jo vertė ir iš pirmų k-1 daiktų reikia surinkti svorį r-svoris[k]
+                );
             else
                 // jei k-asis eksponatas netilptų, jo įtraukti negalima
-                D[k][r] = D[k-1][r];
+                dp[k][r] = dp[k-1][r];
         }
     }
 }
